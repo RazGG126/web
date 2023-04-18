c2ef40e4643013b84486d82d7db679bb09164099
Ivan <ivanakul123@mail.ru>
2023-04-18 18:56:24 +0900

    Fixed some bags.

--- quote_web/__init__.py
+++ quote_web/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 from flask_login import LoginManager
 
 from quote_web.data import db_session
 
 app = Flask(__name__)
+
+run_with_ngrok(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'x@uhg98(FUj9g8f9bz.s'
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from quote_web import app
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run()
